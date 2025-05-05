32080e3bedb0f6eef968eba64daeda3505849654
Gabriel Tessier <gabriel.tessier@grenoble-inp.org>
2025-05-05 14:11:42 +0200

    a

--- src/ycc2rgb.c
+++ src/ycc2rgb.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include "ycc2rgb.h"
 
-rgb_t* ycc2rgb(mcuycc_t *mcu) {
-  
+rgb_t* ycc2rgb(uint8_t y[64], uint8_t cb[64], uint8_t cr[64]) {
+  rgb_t* rgb = malloc(sizeof(
 }
--- src/ycc2rgb.h
+++ src/ycc2rgb.h
@@ -2,18 +2,6 @@
 
 #include <stdint.h>
 
-struct ycc_s {
-  uint8_t y, cb, cr;
-};
-typedef struct ycc_s ycc_t;
-
-struct mcuycc_s {
-  uint8_t y[64];
-  uint8_t cb[64];
-  uint8_t cr[64];
-};
-typedef struct mcuycc_s mcuycc_t;
-
 struct rgb_s {
   uint8_t r, g, b;
 };
