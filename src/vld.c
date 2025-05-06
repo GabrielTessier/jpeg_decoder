fc15bd4444c06232666070c750a6e6130166aa28
Gabriel Tessier <gabriel.tessier@grenoble-inp.org>
2025-05-06 11:21:35 +0200

    correction vld (uint8_t -> int8_t)

--- src/vld.c
+++ src/vld.c
@@ -4,17 +4,17 @@
 #include <stdio.h>
 #include "vld.h"
 
-uint16_t get_val_from_magnitude(uint16_t magnitude, uint16_t indice) {
-  uint16_t min = 1<<(magnitude-1);
-  uint16_t max = min<<1;
+int16_t get_val_from_magnitude(uint16_t magnitude, uint16_t indice) {
+  int16_t min = 1<<(magnitude-1);
+  int16_t max = (min<<1)-1;
   if (indice < min) return indice-max;  // Négatif
   return indice;
 }
 
-uint8_t *decodeDC(huffman_tree_dc_t* ht, FILE* file, uint64_t pos, uint64_t size) {
-  uint8_t diff = 0;
+int8_t *decodeDC(huffman_tree_dc_t* ht, FILE* file, uint64_t pos, uint64_t size) {
+  int8_t diff = 0;
   fseek(file, pos, SEEK_SET);// On se place au début du code
-  uint8_t* res = (uint8_t*) malloc(sizeof(uint8_t)*size); // tableau contenant les dc décodé
+  int8_t* res = (int8_t*) malloc(sizeof(int8_t)*size); // tableau contenant les dc décodé
   uint64_t resi = 0; // indice de où on en est dans res
   huffman_tree_dc_t* symb_decode = ht; // où on en est dans l'arbre de huffman
   // On lit char par char mais on traite bit par bit
@@ -53,10 +53,10 @@ uint8_t *decodeDC(huffman_tree_dc_t* ht, FILE* file, uint64_t pos, uint64_t size
   return res;
 }
 
-uint8_t *decodeAC(huffman_tree_ac_t* ht, FILE* file, uint64_t pos) {
+int8_t *decodeAC(huffman_tree_ac_t* ht, FILE* file, uint64_t pos) {
 
   fseek(file, pos, SEEK_SET);
-  uint8_t *res = (uint8_t*) calloc(63, sizeof(uint8_t));
+  int8_t *res = (int8_t*) calloc(63, sizeof(int8_t));
   uint8_t resi = 0;
   
   huffman_tree_ac_t* symb_decode = ht;
