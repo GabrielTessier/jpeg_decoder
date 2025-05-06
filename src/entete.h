f1daf136056ca8847a8edbe84fa27bb525d6e392
Albin Lelièvre <albin.lelievre@grenoble-inp.org>
2025-05-06 14:41:15 +0200

    entete.h bien

--- src/entete.h
+++ src/entete.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -14,19 +15,19 @@ typedef struct idqtable_s idqtable_t;
 
 struct qtables_s {
     uint8_t nb;
-    idqtable_t *qtables;
+    idqtable_t **qtables;
 };
 typedef struct qtables_s qtables_t;
 
-struct idhtables_s {
+struct idhtable_s {
     uint8_t id;
     huffman_tree_t *htable;
 };
-typedef struct idhtables_s idhtables_t;
+typedef struct idhtable_s idhtable_t;
 
 struct htables_s {
     uint8_t nb;
-    idhtables_t *htables;
+    idhtable_t **htables;
 };
 typedef struct htables_s htables_t;
 
@@ -34,14 +35,16 @@ struct idcomp_s {
     uint8_t idc;
     uint8_t hsampling;
     uint8_t vsampling;
-    uint8_t idh;
+    uint8_t idhdc;
+    uint8_t idhac;
     uint8_t idq;
 };
 typedef struct idcomp_s idcomp_t;
 
 struct comps_s {
     uint8_t nb;
-    idcomp_t *comps;
+    uint8_t ordre[3];
+    idcomp_t **comps;
 };
 typedef struct comps_s comps_t;
 
@@ -53,4 +56,11 @@ struct img_s {
     comps_t *comps;
 };
 typedef struct img_s img_t;
+
+struct couple_tree_depth_s {
+    huffman_tree_t *tree;
+    uint8_t depth;
+};
+typedef struct couple_tree_depth_s couple_tree_depth_t;
+
 
