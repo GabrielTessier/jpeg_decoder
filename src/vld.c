9c55b096270a5c5598dc4a22ddca78fe67e2819f
Gabriel Tessier <gabriel.tessier@grenoble-inp.org>
2025-05-06 13:25:01 +0200

    correction vld.c (car struct modifier)

--- src/vld.c
+++ src/vld.c
@@ -11,12 +11,12 @@ int16_t get_val_from_magnitude(uint16_t magnitude, uint16_t indice) {
   return indice;
 }
 
-int8_t *decodeDC(huffman_tree_dc_t* ht, FILE* file, uint64_t pos, uint64_t size) {
+int8_t *decodeDC(huffman_tree_t* ht, FILE* file, uint64_t pos, uint64_t size) {
   int8_t diff = 0;
   fseek(file, pos, SEEK_SET);// On se place au début du code
   int8_t* res = (int8_t*) malloc(sizeof(int8_t)*size); // tableau contenant les dc décodé
   uint64_t resi = 0; // indice de où on en est dans res
-  huffman_tree_dc_t* symb_decode = ht; // où on en est dans l'arbre de huffman
+  huffman_tree_t* symb_decode = ht; // où on en est dans l'arbre de huffman
   // On lit char par char mais on traite bit par bit
   while (true) {
     char c = fgetc(file);
@@ -28,18 +28,18 @@ int8_t *decodeDC(huffman_tree_dc_t* ht, FILE* file, uint64_t pos, uint64_t size)
       // Si on a atteint une fauille
       if (symb_decode->droit == NULL && symb_decode->gauche == NULL) {
 	// Si la valeur à lire est entièrement sur le char courant
-	if (symb_decode->magnitude + i < 8) {
+	if (symb_decode->symb + i < 8) {
 	  // On récupère la suite de bit sous forme de int puis on incrémente i
-	  uint16_t indice = (c>>(8-i-symb_decode->magnitude)) & ((1<<symb_decode->magnitude)-1);
-	  res[resi] = diff + get_val_from_magnitude(symb_decode->magnitude, indice);
+	  uint16_t indice = (c>>(8-i-symb_decode->symb)) & ((1<<symb_decode->symb)-1);
+	  res[resi] = diff + get_val_from_magnitude(symb_decode->symb, indice);
 	} else {
 	  // Si la valeur à lire est sur deux char
 	  // On lit les bits de poids fort puis ceux de poids faible
-	  uint16_t indice = (c & (1<<(7-i)))<<(symb_decode->magnitude-(8-i));
+	  uint16_t indice = (c & (1<<(7-i)))<<(symb_decode->symb-(8-i));
 	  c = fgetc(file);
-	  indice += c>>(8-(symb_decode->magnitude-(8-i)));
-	  i = symb_decode->magnitude-(8-i);
-	  res[resi] = diff + get_val_from_magnitude(symb_decode->magnitude, indice);
+	  indice += c>>(8-(symb_decode->symb-(8-i)));
+	  i = symb_decode->symb-(8-i);
+	  res[resi] = diff + get_val_from_magnitude(symb_decode->symb, indice);
 	}
 	diff = res[resi];
 	resi++;
@@ -53,13 +53,13 @@ int8_t *decodeDC(huffman_tree_dc_t* ht, FILE* file, uint64_t pos, uint64_t size)
   return res;
 }
 
-int8_t *decodeAC(huffman_tree_ac_t* ht, FILE* file, uint64_t pos) {
+int8_t *decodeAC(huffman_tree_t* ht, FILE* file, uint64_t pos) {
 
   fseek(file, pos, SEEK_SET);
   int8_t *res = (int8_t*) calloc(63, sizeof(int8_t));
   uint8_t resi = 0;
   
-  huffman_tree_ac_t* symb_decode = ht;
+  huffman_tree_t* symb_decode = ht;
   while (true) {
     char c = fgetc(file);
     int i=0;
