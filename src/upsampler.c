1f34d3c6d1f52c2e10e5e17431761b3e9eb72857
Gabriel Tessier <gabriel.tessier@grenoble-inp.org>
2025-05-06 16:11:59 +0200

    modif upsampler

--- src/upsampler.c
+++ src/upsampler.c
@@ -2,7 +2,7 @@
 #include "jpeg2ppm.h"
 #include "upsampler.h"
 
-bloct_t ***upsampler(bloct_t **cb, bloct_t **cr, uint64_t nbBlockH, uint64_t nbBlockV, idcomp_t* yfact, idcomp_t* cbfact, idcomp_t* crfact) {
+bloct_t ***upsampler(bloct_t **cb, bloct_t **cr, uint64_t nbBlockCbH, uint64_t nbBlockCbV, uint64_t nbBlockCrH, uint64_t nbBlockCrV, idcomp_t* yfact, idcomp_t* cbfact, idcomp_t* crfact) {
   bloct_t ***res = (bloct_t***) malloc(sizeof(bloct_t**)*2);
   uint8_t nbBlockInMCU = yfact->hsampling * yfact->vsampling;
   uint8_t nbBlockInMCUcb = cbfact->hsampling * cbfact->vsampling;
@@ -11,22 +11,43 @@ bloct_t ***upsampler(bloct_t **cb, bloct_t **cr, uint64_t nbBlockH, uint64_t nbB
   uint8_t facteurcr = nbBlockInMCU/nbBlockInMCUcr;
   res[0] = (bloct_t**) malloc(sizeof(bloct_t*)*facteurcb);
   res[1] = (bloct_t**) malloc(sizeof(bloct_t*)*facteurcr);
-  // CB
-  uint8_t facteurH = yfact->hsampling / cbfact->hsampling;
-  uint8_t facteurV = yfact->vsampling / cbfact->vsampling;
-  for (uint64_t i=0; i<nbBlockH; i++) {
-    for (uint64_t j=0; j<nbBlockV; j++) {
+  
+  uint8_t facteurCbH = yfact->hsampling / cbfact->hsampling;
+  uint8_t facteurCbV = yfact->vsampling / cbfact->vsampling;
+  uint8_t facteurCrH = yfact->hsampling / cbfact->hsampling;
+  uint8_t facteurCrV = yfact->vsampling / cbfact->vsampling;
+  
+  for (uint64_t i=0; i<nbBlockCbH; i++) {
+    for (uint64_t j=0; j<nbBlockCbV; j++) {
       for (uint8_t k=0; k<8; k++) {
 	for (uint8_t l=0; l<8; l++) {
 	  uint64_t px = i*8+k;
 	  uint64_t py = j*8+l;
-	  uint64_t dpx = px*facteurH;
-	  uint64_t dpy = px*facteurV;
-	  for (uint8_t offX=0; offX<facteurH; offX++)
-	    for (uint8_t offY=0; offY<facteurV; offY++)
-	      res[0][((dpy+offY)/8)*nbBlockH*facteurH+((dpx+offX)/8)]->data[(dpx+offX)%8][(dpy+offY)%8] = cb[j*nbBlockH+i]->data[k][l];
+	  uint64_t dpx = px*facteurCbH;
+	  uint64_t dpy = px*facteurCbV;
+	  for (uint8_t offX=0; offX<facteurCbH; offX++)
+	    for (uint8_t offY=0; offY<facteurCbV; offY++)
+	      res[0][((dpy+offY)/8)*nbBlockCbH*facteurCbH+((dpx+offX)/8)]->data[(dpx+offX)%8][(dpy+offY)%8] = cb[j*nbBlockCbH+i]->data[k][l];
 	}
       }
     }
   }
+
+  for (uint64_t i=0; i<nbBlockCrH; i++) {
+    for (uint64_t j=0; j<nbBlockCrV; j++) {
+      for (uint8_t k=0; k<8; k++) {
+	for (uint8_t l=0; l<8; l++) {
+	  uint64_t px = i*8+k;
+	  uint64_t py = j*8+l;
+	  uint64_t dpx = px*facteurCrH;
+	  uint64_t dpy = px*facteurCrV;
+	  for (uint8_t offX=0; offX<facteurCrH; offX++)
+	    for (uint8_t offY=0; offY<facteurCrV; offY++)
+	      res[1][((dpy+offY)/8)*nbBlockCrH*facteurCrH+((dpx+offX)/8)]->data[(dpx+offX)%8][(dpy+offY)%8] = cr[j*nbBlockCrH+i]->data[k][l];
+	}
+      }
+    }
+  }
+
+  return res;
 }
--- src/upsampler.h
+++ src/upsampler.h
@@ -2,4 +2,4 @@
 
 #include "jpeg2ppm.h"
 
-bloct_t ***upsampler(bloct_t **cb, bloct_t **cr, uint64_t nbBlockH, uint64_t nbBlockV, idcomp_t* yfact, idcomp_t* cbfact, idcomp_t* crfact);
+bloct_t ***upsampler(bloct_t **cb, bloct_t **cr, uint64_t nbBlockCbH, uint64_t nbBlockCbV, uint64_t nbBlockCrH, uint64_t nbBlockCrV, idcomp_t* yfact, idcomp_t* cbfact, idcomp_t* crfact);
