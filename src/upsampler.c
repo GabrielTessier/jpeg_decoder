3e66a114afc24fd8610f61e49fc9d4686ec4d727
Philippe Belda <philippe.belda@grenoble-inp.org>
2025-05-07 17:41:59 +0200

    upsampler commentaires

--- src/upsampler.c
+++ src/upsampler.c
@@ -3,9 +3,10 @@
 #include "entete.h"
 #include "upsampler.h"
 
+// Retourne un tableau 1x2 {Blocs(Cb), Blocs(Cr)} upsamplés
 bloctu8_t ***upsampler(bloctu8_t **cb, bloctu8_t **cr, uint64_t nbBlockCbH, uint64_t nbBlockCbV, uint64_t nbBlockCrH, uint64_t nbBlockCrV, idcomp_t* yfact, idcomp_t* cbfact, idcomp_t* crfact) {
   bloctu8_t ***res = (bloctu8_t***) malloc(sizeof(bloctu8_t**)*2);
-  uint8_t nbBlockInMCU = yfact->hsampling * yfact->vsampling;
+  uint8_t nbBlockInMCU   = yfact->hsampling * yfact->vsampling;
   uint8_t nbBlockInMCUcb = cbfact->hsampling * cbfact->vsampling;
   uint8_t nbBlockInMCUcr = crfact->hsampling * crfact->vsampling;
   uint8_t facteurcb = nbBlockInMCU/nbBlockInMCUcb;
@@ -18,18 +19,18 @@ bloctu8_t ***upsampler(bloctu8_t **cb, bloctu8_t **cr, uint64_t nbBlockCbH, uint
   uint8_t facteurCrH = yfact->hsampling / cbfact->hsampling;
   uint8_t facteurCrV = yfact->vsampling / cbfact->vsampling;
   
-  for (uint64_t i=0; i<nbBlockCbH; i++) {
-    for (uint64_t j=0; j<nbBlockCbV; j++) {
-      for (uint8_t k=0; k<8; k++) {
-	for (uint8_t l=0; l<8; l++) {
-	  uint64_t px = i*8+k;
-	  uint64_t py = j*8+l;
-	  uint64_t dpx = px*facteurCbH;
-	  uint64_t dpy = py*facteurCbV;
-	  for (uint8_t offX=0; offX<facteurCbH; offX++)
-	    for (uint8_t offY=0; offY<facteurCbV; offY++)
-	      res[0][((dpy+offY)/8)*nbBlockCbH*facteurCbH+((dpx+offX)/8)]->data[(dpx+offX)%8][(dpy+offY)%8] = cb[j*nbBlockCbH+i]->data[k][l];
-	}
+  for (uint64_t i=0; i<nbBlockCbH; i++) {   // parcours des colonnes de blocs dans cb
+    for (uint64_t j=0; j<nbBlockCbV; j++) { // parcours des lignes de blocs dans cb
+      for (uint8_t k=0; k<8; k++) {         // parcours des colonnes d'un bloc
+        for (uint8_t l=0; l<8; l++) {       // parcours des lignes d'un bloc
+          uint64_t px = i*8+k;              // position x du pixel considéré dans cb
+          uint64_t py = j*8+l;              // position y du pixel considéré dans cb
+          uint64_t dpx = px*facteurCbH;     // position x du pixel considéré dans res
+          uint64_t dpy = py*facteurCbV;     // position y du pixel considéré dans res
+          for (uint8_t offX=0; offX<facteurCbH; offX++)   // parcours des colonnes où imprimer le pixel considéré dans res
+            for (uint8_t offY=0; offY<facteurCbV; offY++) // parcours des lignes où imprimer le pixel considéré dans res
+              res[0][((dpy+offY)/8)*(nbBlockCbH*facteurCbH) + ((dpx+offX)/8)]->data[(dpx+offX)%8][(dpy+offY)%8] = cb[j*nbBlockCbH+i]->data[k][l];
+        }
       }
     }
   }
@@ -37,15 +38,15 @@ bloctu8_t ***upsampler(bloctu8_t **cb, bloctu8_t **cr, uint64_t nbBlockCbH, uint
   for (uint64_t i=0; i<nbBlockCrH; i++) {
     for (uint64_t j=0; j<nbBlockCrV; j++) {
       for (uint8_t k=0; k<8; k++) {
-	for (uint8_t l=0; l<8; l++) {
-	  uint64_t px = i*8+k;
-	  uint64_t py = j*8+l;
-	  uint64_t dpx = px*facteurCrH;
-	  uint64_t dpy = py*facteurCrV;
-	  for (uint8_t offX=0; offX<facteurCrH; offX++)
-	    for (uint8_t offY=0; offY<facteurCrV; offY++)
-	      res[1][((dpy+offY)/8)*nbBlockCrH*facteurCrH+((dpx+offX)/8)]->data[(dpx+offX)%8][(dpy+offY)%8] = cr[j*nbBlockCrH+i]->data[k][l];
-	}
+        for (uint8_t l=0; l<8; l++) {
+          uint64_t px = i*8+k;
+          uint64_t py = j*8+l;
+          uint64_t dpx = px*facteurCrH;
+          uint64_t dpy = py*facteurCrV;
+          for (uint8_t offX=0; offX<facteurCrH; offX++)
+            for (uint8_t offY=0; offY<facteurCrV; offY++)
+              res[1][((dpy+offY)/8)*(nbBlockCrH*facteurCrH) + ((dpx+offX)/8)]->data[(dpx+offX)%8][(dpy+offY)%8] = cr[j*nbBlockCrH+i]->data[k][l];
+        }
       }
     }
   }
--- src/upsampler.h
+++ src/upsampler.h
@@ -3,4 +3,15 @@
 #include "entete.h"
 #include "jpeg2ppm.h"
 
+/* Entrées :
+ * cb         : tableau 8x8 contenant les Cb
+ * cr         : tableau 8x8 contenant les Cr
+ * nbBlockCbH : nombre de blocs horizontaux de Cb
+ * nbBlockCbV : nombre de blocs verticaux de Cb
+ * nbBlockCrH : nombre de blocs horizontaux de Cr
+ * nbBlockCrV : nombre de blocs verticaux de Cr
+ * yfact      : structure contenant les infos de Y
+ * cbfact     : structure contenant les infos de Cb
+ * crfact     : structure contenant les infos de Cr
+ */
 bloctu8_t ***upsampler(bloctu8_t **cb, bloctu8_t **cr, uint64_t nbBlockCbH, uint64_t nbBlockCbV, uint64_t nbBlockCrH, uint64_t nbBlockCrV, idcomp_t* yfact, idcomp_t* cbfact, idcomp_t* crfact);
