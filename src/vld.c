0ff60abf8f019dd6f55316096b9b60c0fdceb0ef
Gabriel Tessier <gabriel.tessier@grenoble-inp.org>
2025-05-09 12:17:13 +0200

    modif calcul dc ac

--- src/vld.c
+++ src/vld.c
@@ -24,16 +24,16 @@ char my_getc(FILE* file, char old) {
   return c;
 }
 
-blocl16_t *decode_bloc_acdc(FILE *fichier, huffman_tree_t *hdc, huffman_tree_t *hac, int16_t *dc_prec, uint64_t *debut, uint8_t *off) {
-  int16_t *sousdc = decodeDC(hdc, fichier, *debut, off, 1);
+blocl16_t *decode_bloc_acdc(FILE *fichier, huffman_tree_t *hdc, huffman_tree_t *hac, int16_t *dc_prec, uint8_t *off) {
+  int16_t *sousdc = decode_list_coef(hdc, fichier, off, DC);
   int16_t dc = sousdc[0];
   dc += *dc_prec; // calcul du coef à partir de la différence
   *dc_prec = dc;
 
-  *debut = ftell(fichier)-1; // début de AC
-  int16_t *ac = decodeAC(hac, fichier, *debut, off);
+  fseek(fichier, -1, SEEK_CUR);
+  int16_t *ac = decode_list_coef(hac, fichier, off, AC);
 
-  *debut = ftell(fichier)-1;
+  fseek(fichier, -1, SEEK_CUR);
   // écriture des DC, AC
   blocl16_t * bloc = (blocl16_t*) malloc(sizeof(blocl16_t));
   bloc->data[0] = dc;
@@ -44,17 +44,65 @@ blocl16_t *decode_bloc_acdc(FILE *fichier, huffman_tree_t *hdc, huffman_tree_t *
   return bloc;
 }
 
-int16_t *decodeDC(huffman_tree_t* ht, FILE* file, uint64_t pos, uint8_t *off, uint64_t size) {
-  int16_t diff = 0;
-  fseek(file, pos, SEEK_SET);// On se place au début du code
-  int16_t* res = (int16_t*) malloc(sizeof(int16_t)*size); // tableau contenant les dc décodé
+int16_t read_val_from_magnitude(FILE* file, uint8_t magnitude, uint8_t *off, char *c) {
+  if (magnitude + *off < 8) {
+    // On récupère la suite de bit sous forme de int puis on incrémente i
+    uint16_t indice = (*c>>(7-*off-magnitude)) & ((1<<magnitude)-1);
+    *off += magnitude;
+    return get_val_from_magnitude(magnitude, indice);
+  } else {
+    // Si la valeur à lire est sur deux char
+    // On lit les bits de poids fort puis ceux de poids faible
+    uint16_t indice = (*c & ((1<<(7-*off))-1))<<(magnitude-(7-*off));
+    //c = fgetc(file);
+    *c = my_getc(file, *c);
+    if (magnitude + *off < 16) {
+      indice += (*c&((1<<8)-1))>>(8-(magnitude-(7-*off)));
+      *off = magnitude-(7-*off)-1;
+      return get_val_from_magnitude(magnitude, indice);
+    } else {
+      indice += (*c&((1<<8)-1))<<(magnitude-(7-*off)-8);
+      *c = my_getc(file, *c);
+      indice += (*c&((1<<8)-1))>>(8-(magnitude-(7-*off)-8));
+      *off = (magnitude-(7-*off)-8)-1;
+      return get_val_from_magnitude(magnitude, indice);
+    }
+  }
+}
+
+int16_t decode_coef_DC(FILE *file, huffman_tree_t *symb_decode, uint8_t *off, char *c) {
+  return read_val_from_magnitude(file, symb_decode->symb, off, c);
+}
+
+void decode_coef_AC(FILE *file, huffman_tree_t *symb_decode, int16_t *res,
+                    uint64_t *resi, uint8_t *off, char *c) {
+  switch (symb_decode->symb) {
+  case (uint8_t) 0x00:
+    *resi = 63;
+    break;
+  case (uint8_t) 0xf0:
+    *resi += 16;
+    break;
+  default:
+    uint8_t alpha = symb_decode->symb >> 4;
+    uint8_t gamma = symb_decode->symb & 0b00001111;
+    *resi += alpha;
+    res[*resi] = read_val_from_magnitude(file, gamma, off, c);
+    (*resi)++;
+    break;
+  }
+}
+
+int16_t *decode_list_coef(huffman_tree_t* ht, FILE* file, uint8_t *off, enum acdc_e type) {
+  uint64_t size = 0;
+  if (type==AC) size = 63;
+  else size = 1;
+  int16_t* res = (int16_t*) calloc(size, sizeof(int16_t)); // tableau contenant les dc décodé
   uint64_t resi = 0; // indice de où on en est dans res
   huffman_tree_t* symb_decode = ht; // où on en est dans l'arbre de huffman
   // On lit char par char mais on traite bit par bit
-  int i = *off;
+  uint8_t i = *off;
   char c = fgetc(file);
-  //printf("DCDC : %x, %d\n", c, i);
-  //if (c == (char)0x01 && i == 3) i = 2;
   bool code_que_un = true;
   while (true) {
     while (i<8) {
@@ -67,107 +115,23 @@ int16_t *decodeDC(huffman_tree_t* ht, FILE* file, uint64_t pos, uint8_t *off, ui
       }
       // Si on a atteint une fauille
       if (symb_decode->droit == NULL && symb_decode->gauche == NULL) {
-	if (code_que_un) {
-	  fprintf(stderr, "Le code de huffman avec que des 1 est utilisé\n");
-	  exit(EXIT_FAILURE);
-	}
-	code_que_un = true;
-	// Si la valeur à lire est entièrement sur le char courant
-	if (symb_decode->symb + i < 8) {
-	  // On récupère la suite de bit sous forme de int puis on incrémente i
-	  uint16_t indice = (c>>(7-i-symb_decode->symb)) & ((1<<symb_decode->symb)-1);
-	  i += symb_decode->symb;
-	  res[resi] = diff + get_val_from_magnitude(symb_decode->symb, indice);
-	} else {
-	  // Si la valeur à lire est sur deux char
-	  // On lit les bits de poids fort puis ceux de poids faible
-	  uint16_t indice = (c & ((1<<(7-i))-1))<<(symb_decode->symb-(7-i));
-	  //c = fgetc(file);
-	  c = my_getc(file, c);
-	  if (symb_decode->symb + i < 16) {
-	    indice += (c&((1<<8)-1))>>(8-(symb_decode->symb-(7-i)));
-	    i = symb_decode->symb-(7-i)-1;
-	    res[resi] = diff + get_val_from_magnitude(symb_decode->symb, indice);
-	  } else {
-	    indice += (c&((1<<8)-1))<<(symb_decode->symb-(7-i)-8);
-	    c = my_getc(file, c);
-	    indice += (c&((1<<8)-1))>>(8-(symb_decode->symb-(7-i)-8));
-	    i = (symb_decode->symb-(7-i)-8)-1;
-	    res[resi] = diff + get_val_from_magnitude(symb_decode->symb, indice);
+	if (type == DC) {
+	  if (code_que_un) {
+	    fprintf(stderr, "Le code de huffman avec que des 1 est utilisé\n");
+	    exit(EXIT_FAILURE);
 	  }
-	}
-	diff = res[resi];
-	resi++;
-	symb_decode = ht;
-	if (resi == size) break;
-      }
-      i++;
-    }
-    if (resi == size) break;
-    //c = fgetc(file);
-    c = my_getc(file, c);
-    i = 0;
-  }
-  *off = i+1;
-  return res;
-}
-
-int16_t *decodeAC(huffman_tree_t* ht, FILE* file, uint64_t pos, uint8_t *off) {
-  fseek(file, pos, SEEK_SET);
-  int16_t *res = (int16_t*) calloc(63, sizeof(int16_t));
-  uint8_t resi = 0;
-  
-  huffman_tree_t* symb_decode = ht;
-  int i = *off;
-  char c = fgetc(file);
-  while (true) {
-    while (i<8) {
-      // on regarde si le bit courant est 0 ou 1
-      if (((c>>(7-i)) & 1) == 1) symb_decode = symb_decode->droit;
-      else symb_decode = symb_decode->gauche;
-      // Si on a atteint une fauille
-      if (symb_decode->droit == NULL && symb_decode->gauche == NULL) {
-	switch (symb_decode->symb) {
-	case (uint8_t) 0x00:
-	  resi = 63;
-	  break;
-	case (uint8_t) 0xf0:
-	  resi += 16;
-	  break;
-	default:
-	  uint8_t alpha = symb_decode->symb >> 4;
-	  uint8_t gamma = symb_decode->symb & 0b00001111;
-	  resi += alpha;
-	  if (gamma + i < 8) {
-	    uint16_t indice = (c>>(7-i-gamma)) & ((1<<gamma)-1);
-	    i += gamma;
-	    res[resi] = get_val_from_magnitude(gamma, indice);
-	  } else {
-	    uint16_t indice = (c & ((1<<(7-i))-1))<<(gamma-(7-i));
-	    //c = fgetc(file);
-	    c = my_getc(file, c);
-	    if (gamma + i < 16) {
-	      indice += (c&((1<<8)-1))>>(8-(gamma-(7-i)));
-	      i = gamma-(7-i)-1;
-	      res[resi] = get_val_from_magnitude(gamma, indice);
-	    } else {
-	      indice += (c&((1<<8)-1))<<(gamma-(7-i)-8);
-	      c = my_getc(file, c);
-	      indice += (c&((1<<8)-1))>>(8-(gamma-(7-i)-8));
-	      i = (gamma-(7-i)-8)-1;
-	      res[resi] = get_val_from_magnitude(gamma, indice);
-	    }
-	  }
+	  res[resi] = decode_coef_DC(file, symb_decode, &i, &c);
+	  code_que_un = true;
 	  resi++;
-	  break;
+	} else {
+	  decode_coef_AC(file, symb_decode, res, &resi, &i, &c);
 	}
 	symb_decode = ht;
-	if (resi >= 63) break;
+	if (resi >= size) break;
       }
       i++;
     }
-    if (resi >= 63) break;
-    //c = fgetc(file);
+    if (resi >= size) break;
     c = my_getc(file, c);
     i = 0;
   }
--- src/vld.h
+++ src/vld.h
@@ -10,19 +10,8 @@ struct huffman_tree_s {
 };
 typedef struct huffman_tree_s huffman_tree_t;
 
+enum acdc_e { AC, DC };
 
-blocl16_t *decode_bloc_acdc(FILE *fichier, huffman_tree_t *hdc, huffman_tree_t *hac, int16_t *dc_prec, uint64_t *debut, uint8_t *off);
+int16_t *decode_list_coef(huffman_tree_t* ht, FILE* file, uint8_t *off, enum acdc_e type);
 
-/* Décode tous les DC
- * ht : arbre de huffman
- * file : fichier
- * pos : position du début du code dans le fichier
- * size : nombre de dc à décoder */
-int16_t *decodeDC(huffman_tree_t* ht, FILE* file, uint64_t pos, uint8_t *off, uint64_t size);
-
-
-/* Décode un macrobloc
- * ht : arbre de huffman
- * file : fichier
- * pos : position du début du code dans le fichier */
-int16_t *decodeAC(huffman_tree_t* ht, FILE* file, uint64_t pos, uint8_t *off);
+blocl16_t *decode_bloc_acdc(FILE *fichier, huffman_tree_t *hdc, huffman_tree_t *hac, int16_t *dc_prec, uint8_t *off);
