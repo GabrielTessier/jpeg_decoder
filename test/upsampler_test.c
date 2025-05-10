1064c7a0a58161177fa625f20d47c70ae3edc98a
phil <belda.philippe@proton.me>
2025-05-10 10:34:03 +0200

    test upsampler en cours

---
+++ test/upsampler_test.c
@@ -0,0 +1,10 @@
+int main(int argc, char *argv[]) {
+  int facteurH[4] = {0, 1, 2, 4};
+  int facteurV[4] = {0, 1, 2, 4};
+  
+  for (int i=0; i<4; i++) {
+    for (int j=0; j<4; j++) {
+      
+    }
+  }
+}
