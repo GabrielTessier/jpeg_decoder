f69e7b3ef14281835d10b97858dddec4eb25d124
Gabriel <gabriel.tessier45@gmail.com>
2025-05-16 21:23:13 +0200

    test .gitlab-ci.yml

--- test/entete_test.c
+++ test/entete_test.c
@@ -176,8 +176,7 @@ static uint8_t parcours_hufftree(huffman_tree_t ht, char *path) {
 
 static bool parse_comp_hufftables_blabla(char *nom_fichier, htables_t hts) {
    char str[100];
-   system("pwd");
-   sprintf(str, "./bin/jpeg2blabla %s > /tmp/blabla_output.txt", nom_fichier);
+   sprintf(str, "jpeg2blabla %s > /tmp/blabla_output.txt", nom_fichier);
    system(str);
    FILE *file = fopen("/tmp/blabla_output.txt", "r");
    size_t line_size_max = 100;
