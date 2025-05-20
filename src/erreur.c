50f43d46dfea1ae9ae9898bab8d16d2ee7cac376
Gabriel Tessier <gabriel.tessier@grenoble-inp.org>
2025-05-20 14:05:00 +0200

    couleur

--- src/erreur.c
+++ src/erreur.c
@@ -5,6 +5,6 @@
 #include <erreur.h>
 
 void print_erreur(const erreur_t err) {
-   fprintf(stderr, RED "ERREUR %d" RESET " : %s\n", err.code, err.com);
+   fprintf(stderr, BOLDRED "ERREUR %d" RESET " : %s\n", err.code, err.com);
    if (err.must_free) free(err.com);
 }
--- test/test_utils.c
+++ test/test_utils.c
@@ -1,12 +1,9 @@
 #include <stdbool.h>
 #include <stdarg.h>
 
+#include <colors.h>
 #include <stdio.h>
 #include "test_utils.h"
-
-#define RED_BOLD   "\e[1;31m"
-#define GREEN_BOLD "\e[1;32m"
-#define ANSI_RESET "\x1b[0m"
 
 void test_res(bool test_var, char *argv[], char *format, ...) {
   va_list args;
@@ -15,8 +12,8 @@ void test_res(bool test_var, char *argv[], char *format, ...) {
   vsprintf(str, format, args);
   va_end(args);
   if (test_var) {
-    printf("%s %s\r\t\t\tPASSED%s %s\n", argv[0], GREEN_BOLD, ANSI_RESET, str);
+     printf("%s %s\r\t\t\tPASSED%s %s\n", argv[0], BOLDGREEN, RESET, str);
   } else {
-    printf("%s %s\r\t\t\tFAILED%s %s\n", argv[0], RED_BOLD, ANSI_RESET, str);
+    printf("%s %s\r\t\t\tFAILED%s %s\n", argv[0], BOLDRED, RESET, str);
   }
 }
